b55b3a2d43052fc8ecb87cd27eb7df705fbd18cd
Matias Contreras <matiasleoncontreras@gmail.com>
2025-10-05 15:05:18 -0300

    change activity name

--- app/src/main/java/com/example/clubdeportivoapp/GestionSociosActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/GestionSociosActivity.kt
@@ -10,7 +10,7 @@ class GestionSociosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_socio)
+        setContentView(R.layout.activity_gestion_socio)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
